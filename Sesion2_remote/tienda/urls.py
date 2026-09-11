b962adc0ed90f5e2173503d8cd42ab6ab53f02cb
magaly-rosales <magaly.rosales@tecsup.edu.pe>
2026-09-10 20:53:28 -0500

    chore: remover archivos personales del repositorio (opencode, notas, duplicados)

--- Sesion2_remote/tienda/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.listado_productos, name='listado_productos'),
-    path('crear/', views.crear_producto, name='crear_producto'),
-]
